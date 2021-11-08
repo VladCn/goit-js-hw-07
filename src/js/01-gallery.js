95fb8aaba63b2fc039e367d6ac19af6310a00171
Vladislav Zaitsev <vladislav.zaytcev84@gmail.com>
2021-11-08 19:08:53 +0200

    hw_07_ver1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,3 +1,34 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
-console.log(galleryItems);
+// console.log(galleryItems);
+
+const conteiner = document.querySelector('.gallery')
+const cards = createColorCards(galleryItems)
+
+conteiner.insertAdjacentHTML("beforeend", cards)
+conteiner.addEventListener("click", openModal)
+function openModal(evt){
+    evt.preventDefault()
+    console.log(evt.target)
+}
+
+function createColorCards(gallery){
+    const markup = gallery.map(({preview, original, description}) => {
+        console.log(description)
+        return`
+        <div class="gallery__item">
+            <a class="gallery__link" href="${original}">
+                <img
+                    class="gallery__image"
+                    src="${preview}"
+                    data-source="${original}"
+                    alt="${description}"
+                />
+            </a>
+        </div>`
+        })
+    return markup.join('')
+}
+
+createColorCards(galleryItems)
+
