db0f2f283a5ed70800cc5b990fce99acd0aef774
Vladislav Zaitsev <vladislav.zaytcev84@gmail.com>
2021-11-09 15:07:32 +0200

    hw_07_ver2

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -4,12 +4,42 @@ import { galleryItems } from './gallery-items.js';
 
 const conteiner = document.querySelector('.gallery')
 const cards = createColorCards(galleryItems)
+// const modal = createModal()
 
 conteiner.insertAdjacentHTML("beforeend", cards)
+// conteiner.insertAdjacentHTML("beforeend", createModal())
 conteiner.addEventListener("click", openModal)
+
 function openModal(evt){
     evt.preventDefault()
-    console.log(evt.target)
+    if(!evt.target.classList.contains("gallery__image")){
+        return;
+    }
+    const templateNode = document.querySelector('template')
+    if (templateNode) {
+        templateNode.remove()
+    }
+
+    const origPic = evt.target.getAttribute("data-source")
+    const origDisc = evt.target.getAttribute("alt")
+    const modalCreate = `<template>
+            <div
+            class = "modal" >
+            <img
+            class = "gallery__image"
+            src = "${origPic}"
+            alt = "${origDisc}"
+            />
+            </div>
+            </template>`
+
+
+    conteiner.insertAdjacentHTML("beforeend", modalCreate)
+
+    const instance = basicLightbox.create(
+        document.querySelector('template')
+    )
+    instance.show()
 }
 
 function createColorCards(gallery){
@@ -17,6 +47,7 @@ function createColorCards(gallery){
         console.log(description)
         return`
         <div class="gallery__item">
+            
             <a class="gallery__link" href="${original}">
                 <img
                     class="gallery__image"
@@ -24,11 +55,24 @@ function createColorCards(gallery){
                     data-source="${original}"
                     alt="${description}"
                 />
-            </a>
+                 
+            </a>       
         </div>`
         })
+
     return markup.join('')
 }
 
 createColorCards(galleryItems)
 
+
+// < template >
+// < div
+// className = "modal" >
+//     < img
+// className = "gallery__image"
+// src = "${original}"
+// alt = "${description}"
+//     / >
+//     < /div>
+// </template>
