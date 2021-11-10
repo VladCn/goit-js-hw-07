b41eaffe22fcd32cf584967eed1a8cad133fd480
Vladislav Zaitsev <vladislav.zaytcev84@gmail.com>
2021-11-10 12:59:15 +0200

    hw_07_ver2.1

--- src/js/02-lightbox.js
+++ src/js/02-lightbox.js
@@ -1,4 +1,27 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+
+const conteiner = document.querySelector('.gallery')
+const cards = createColorCards(galleryItems)
+
+conteiner.insertAdjacentHTML("beforeend", cards)
+
+new SimpleLightbox('.gallery a', {captionsData: "alt"});
+
+
+function createColorCards(gallery){
+    const markup = gallery.map(({preview, original, description}) => {
+
+        return`
+        <div class="gallery__item">  
+        <a class="gallery__item" href="${original}">
+            <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>
+        </div>`
+    })
+
+    return markup.join('')
+}
+
+createColorCards(galleryItems)
